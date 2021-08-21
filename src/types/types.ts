31b3f1a9c93f199c8c6ee791c51d84ab91688a7a
xieguigang <xie.guigang@gcmodeller.org>
2021-08-21 22:38:07 +0800

    add source from https://github.com/cheminfo/netcdfjs

---
+++ src/types/types.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+namespace type {
+
+    export const types = {
+        BYTE: 1,
+        CHAR: 2,
+        SHORT: 3,
+        INT: 4,
+        FLOAT: 5,
+        DOUBLE: 6
+    };
+}
